423974d7f1526e1315a4eb7ce61944cc2c8d6721
Jayant <jayantmehra02@gmail.com>
2021-08-13 01:32:21 +0530

    fixed top margin

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -7,7 +7,7 @@ const CharacterList = ({ selectedCharacters, isLoading }) => {
       {isLoading ? (
         <Loading />
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-20 gap-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-20 gap-2 mt-10">
           {selectedCharacters.length > 0 &&
             selectedCharacters.map((character) => {
               return <SingleCharacter key={character.char_id} {...character} />;
